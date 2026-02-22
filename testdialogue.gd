2d2085bd3ee6785d7f8d9e994640b7a1d7033cff
driftingdeluge <DriftingDeluge@gmail.com>
2026-02-21 16:26:58 -0800

    add sound

--- testdialogue.gd
+++ testdialogue.gd
@@ -1,8 +1,18 @@
 extends Node2D
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
+@onready var yes_sound: AudioStreamPlayer = $YesSounds
+@onready var no_sound: AudioStreamPlayer = $NoSounds
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+	print(Dialogic.get_signal_list())
+
+func _on_dialogic_signal(argument):
+	if argument == "choice_yes":
+		$YesSounds.play()
+	elif argument == "choice_no":
+		$NoSounds.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,5 +23,5 @@ func _input(event: InputEvent):
 		return
 	
 	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
-		Dialogic.start('Dialogue Intro')
+		Dialogic.start('Dialogue Waiter')
 		get_viewport().set_input_as_handled()
